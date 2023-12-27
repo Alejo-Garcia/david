048cbe3746bdaa3cef50bc33c0954a55fdacf62f
alejo-garcia <aign93@gmail.com>
2023-12-27 13:35:06 -0300

    feat: update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,33 @@
 module.exports = {
+  env: {
+    'jest/globals': true,
+  },
+  extends: ['@react-native', 'plugin:typescript-sort-keys/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['import', 'sort-keys-fix', 'sort-destructure-keys', 'jest'],
   root: true,
-  extends: '@react-native',
+  rules: {
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          caseInsensitive: true,
+          order: 'asc',
+        },
+        distinctGroup: true,
+        'newlines-between': 'always',
+      },
+    ],
+    'react/jsx-sort-props': [
+      2,
+      {
+        callbacksLast: true,
+        ignoreCase: true,
+        reservedFirst: true,
+        shorthandLast: true,
+      },
+    ],
+    'sort-destructure-keys/sort-destructure-keys': 2,
+    'sort-keys-fix/sort-keys-fix': 'error',
+  },
 };
