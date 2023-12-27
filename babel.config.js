6dfa13ce62f9f7354f3c581213d362f558e08064
alejo-garcia <aign93@gmail.com>
2023-12-27 16:59:03 -0300

    refactor: add barrelling to screens

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,14 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
       'module-resolver',
       {
         alias: {
           '@assets': './assets',
+          '@screens': './src/screens',
         },
       },
     ],
   ],
+  presets: ['module:@react-native/babel-preset'],
 };
