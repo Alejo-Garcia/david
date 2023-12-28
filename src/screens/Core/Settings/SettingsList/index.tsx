83b42b8162952434f17da522f2b8c33b66f76c56
alejo-garcia <aign93@gmail.com>
2023-12-28 03:03:25 -0300

    feat: add SettingsList screen

--- src/screens/Core/Settings/SettingsList/index.tsx
+++ src/screens/Core/Settings/SettingsList/index.tsx
@@ -1,11 +1,36 @@
-import {SafeAreaView, Text, View} from 'react-native';
+import {Setting} from '@components';
+import {SafeAreaView, ScrollView, View} from 'react-native';
+
+import styles from './styles';
 
 export function SettingsList(): JSX.Element {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>{'SettingsList'}</Text>
-      </View>
+    <SafeAreaView style={styles.viewMain}>
+      <ScrollView
+        contentContainerStyle={styles.scrollViewSettings}
+        showsVerticalScrollIndicator={false}>
+        <View style={styles.viewSetting}>
+          <Setting
+            description={'Toggle reception of push notifications'}
+            label={'Push notifications'}
+            isActive
+          />
+        </View>
+
+        <View style={styles.viewSetting}>
+          <Setting
+            description={'Toggle reception of email notifications'}
+            label={'Email notifications'}
+          />
+        </View>
+
+        <View style={styles.viewSetting}>
+          <Setting
+            description={'Toggle sending usage statistics'}
+            label={'Data collection'}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
---
+++ src/screens/Core/Settings/SettingsList/styles.ts
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  scrollViewSettings: {
+    marginHorizontal: 8,
+    marginTop: 8,
+  },
+  viewMain: {
+    backgroundColor: '#19232D',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  viewSetting: {
+    marginBottom: 8,
+  },
+});
+
+export default styles;
