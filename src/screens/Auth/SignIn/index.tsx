c7e84f86935e04bfb589440cfd1612b479bd23bf
alejo-garcia <aign93@gmail.com>
2023-12-28 02:19:08 -0300

    feat: add SignIn screen

--- src/screens/Auth/SignIn/index.tsx
+++ src/screens/Auth/SignIn/index.tsx
@@ -1,19 +1,97 @@
+import {Button, ControllerTextInput} from '@components';
+import {zodResolver} from '@hookform/resolvers/zod';
 import {useNavigation} from '@react-navigation/native';
-import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import {SignInSchema} from '@schemas';
+import {createRef} from 'react';
+import {useForm} from 'react-hook-form';
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+import {TextInput} from 'react-native';
+
+import styles from './styles';
 
 export function SignIn(): JSX.Element {
   const {navigate} = useNavigation();
+  const {
+    control,
+    formState: {errors},
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: zodResolver(SignInSchema),
+  });
+
+  const onSubmit = () => {
+    navigate('CoreStack');
+  };
+
+  const passwordRef = createRef<TextInput>();
 
   return (
-    <SafeAreaView>
-      <View>
-        <TouchableOpacity
-          onPress={() => {
-            navigate('CoreStack');
-          }}>
-          <Text>{'SignIn'}</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.viewMain}>
+      <SafeAreaView>
+        <View style={styles.viewLogo}>
+          <Image
+            resizeMode={'contain'}
+            source={{
+              uri: 'https://upload.wikimedia.org/wikipedia/commons/d/db/Blaze_apostas_logo.png',
+            }}
+            style={styles.imageLogo}
+          />
+        </View>
+
+        <View style={styles.viewTitle}>
+          <Text style={styles.textTitle}>{'Sign in to your account'}</Text>
+        </View>
+
+        <View style={styles.viewForm}>
+          <View>
+            <ControllerTextInput
+              autoCapitalize={'none'}
+              control={control}
+              enterKeyHint={'next'}
+              error={errors.email?.message}
+              keyboardType={'email-address'}
+              name={'email'}
+              placeholder={'Email address'}
+              textContentType={'emailAddress'}
+              onSubmitEditing={() => passwordRef.current?.focus()}
+            />
+          </View>
+
+          <View>
+            <ControllerTextInput
+              ref={passwordRef}
+              control={control}
+              enterKeyHint={'done'}
+              error={errors.password?.message}
+              name={'password'}
+              placeholder={'Password'}
+              textContentType={'password'}
+              secureTextEntry
+            />
+          </View>
+
+          <View style={styles.viewButton}>
+            <Button
+              icon={'arrow-forward-outline'}
+              label={'Sign In'}
+              onPress={handleSubmit(onSubmit)}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
---
+++ src/screens/Auth/SignIn/styles.ts
@@ -0,0 +1,36 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  imageLogo: {
+    height: 80,
+    width: 160,
+  },
+  textTitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  viewButton: {
+    marginTop: 8,
+  },
+  viewForm: {
+    marginHorizontal: 16,
+  },
+  viewLogo: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 16,
+  },
+  viewMain: {
+    backgroundColor: '#19232D',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  viewTitle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 32,
+  },
+});
+
+export default styles;
