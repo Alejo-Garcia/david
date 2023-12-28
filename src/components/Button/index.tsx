a7432867b5006ca1fcb73c7f6fd37b0e819e9cd8
alejo-garcia <aign93@gmail.com>
2023-12-28 03:23:43 -0300

    feat: update Button component disabled styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react';
-import {Pressable, Text} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Props from './props';
@@ -8,24 +8,34 @@ import styles from './styles';
 const initialBGColor = '#F12C4C';
 const pressedBGColor = '#A81E35';
 
-export function Button({icon, label, ...props}: Props): JSX.Element {
+export function Button({
+  disabled,
+  icon,
+  isLoading,
+  label,
+  ...props
+}: Props): JSX.Element {
   const [backgroundColor, setBackgroundColor] = useState(initialBGColor);
 
   return (
-    <Pressable
-      style={[styles.viewMain, {backgroundColor: backgroundColor}]}
-      {...props}
-      onPressIn={() => setBackgroundColor(pressedBGColor)}
-      onPressOut={() => setBackgroundColor(initialBGColor)}>
-      <Text style={styles.textMain}>{label}</Text>
-      {icon && (
-        <Ionicons
-          color={'white'}
-          name={icon}
-          size={18}
-          style={styles.iconMain}
-        />
-      )}
-    </Pressable>
+    <View style={styles.viewMain}>
+      <Pressable
+        disabled={disabled}
+        style={[styles.pressableMain, {backgroundColor: backgroundColor}]}
+        onPressIn={() => setBackgroundColor(pressedBGColor)}
+        onPressOut={() => setBackgroundColor(initialBGColor)}
+        {...props}>
+        <Text style={styles.textMain}>{label}</Text>
+        {icon && !isLoading && (
+          <Ionicons
+            color={'white'}
+            name={icon}
+            size={18}
+            style={styles.iconMain}
+          />
+        )}
+      </Pressable>
+      {disabled && <View style={styles.viewDisabledOverlay} />}
+    </View>
   );
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -4,17 +4,29 @@ const styles = StyleSheet.create({
   iconMain: {
     paddingLeft: 8,
   },
-  textMain: {
-    color: 'white',
-    fontSize: 13,
-    fontWeight: '600',
-  },
-  viewMain: {
+  pressableMain: {
     alignItems: 'center',
     borderRadius: 4,
     flexDirection: 'row',
     height: 48,
     justifyContent: 'center',
+  },
+  textMain: {
+    color: 'white',
+    fontSize: 13,
+    fontWeight: '600',
+  },
+  viewDisabledOverlay: {
+    backgroundColor: 'black',
+    bottom: 0,
+    left: 0,
+    opacity: 0.5,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+  viewMain: {
+    position: 'relative',
   },
 });
 
