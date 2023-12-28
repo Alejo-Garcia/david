7909bb26de2a1b94e7d1143beb83271a9fdb1b26
alejo-garcia <aign93@gmail.com>
2023-12-28 10:25:27 -0300

    feat: add axios query to countries API

--- src/screens/Core/Countries/CountriesList/index.tsx
+++ src/screens/Core/Countries/CountriesList/index.tsx
@@ -1,14 +1,20 @@
-import {Table} from '@components';
+import {ActivityIndicator, StatusIndicator, Table} from '@components';
 import {useHeaderTitle} from '@hooks';
 import {extractCountryValues} from '@utils';
+import useAxios from 'axios-hooks';
 import {useTranslation} from 'react-i18next';
 import {SafeAreaView, ScrollView} from 'react-native';
 
-import countries from './countries.json';
 import styles from './styles';
+
+const queryFields = ['name', 'capital', 'subregion'];
 
 export function CountriesList(): JSX.Element {
   const {t} = useTranslation();
+
+  const [{data, error, loading}, refetch] = useAxios(
+    `/all?fields=${queryFields.join(',')}`,
+  );
 
   useHeaderTitle('CORE.COUNTRIES.COUNTRIES_LIST.HEADER__TITLE');
 
@@ -17,15 +23,32 @@ export function CountriesList(): JSX.Element {
     t('CORE.COUNTRIES.COUNTRIES_LIST.TABLE_HEADER__CAPITAL'),
     t('CORE.COUNTRIES.COUNTRIES_LIST.TABLE_HEADER__REGION'),
   ];
-  const tableData = extractCountryValues(countries);
 
   return (
     <SafeAreaView style={styles.viewMain} testID={'countriesListScreen'}>
       <ScrollView
-        contentContainerStyle={styles.scrollViewSettings}
+        contentContainerStyle={[
+          styles.scrollViewSettings,
+          (loading || error) && styles.scrollViewSettingsStatus,
+        ]}
+        scrollEnabled={!loading || !error}
         showsVerticalScrollIndicator={false}
         testID={'countriesListScrollView'}>
-        <Table tableData={tableData} tableHeader={tableHeader} />
+        {loading ? (
+          <ActivityIndicator color={'#F12C4C'} size={'large'} />
+        ) : error ? (
+          <StatusIndicator
+            buttonIcon={'refresh-outline'}
+            buttonLabel={t('CORE.COUNTRIES.COUNTRIES_LIST.BUTTON__ERROR')}
+            buttonOnPress={() => refetch()}
+            label={t('CORE.COUNTRIES.COUNTRIES_LIST.TEXT__ERROR')}
+          />
+        ) : (
+          <Table
+            tableData={extractCountryValues(data)}
+            tableHeader={tableHeader}
+          />
+        )}
       </ScrollView>
     </SafeAreaView>
   );
--- src/screens/Core/Countries/CountriesList/styles.ts
+++ src/screens/Core/Countries/CountriesList/styles.ts
@@ -5,6 +5,9 @@ const styles = StyleSheet.create({
     marginHorizontal: 8,
     paddingTop: 8,
   },
+  scrollViewSettingsStatus: {
+    flex: 1,
+  },
   tableBorder: {borderColor: '#c8e1ff', borderWidth: 2},
   tableHead: {
     backgroundColor: '#f1f8ff',
@@ -12,6 +15,25 @@ const styles = StyleSheet.create({
   },
   tableHeadText: {
     margin: 6,
+  },
+  textError: {
+    color: 'white',
+    fontSize: 15,
+    lineHeight: 18,
+    textAlign: 'center',
+  },
+  viewActivityIndicator: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  viewButtonError: {
+    marginTop: 16,
+  },
+  viewError: {
+    alignSelf: 'center',
+    flex: 1,
+    justifyContent: 'center',
   },
   viewMain: {
     backgroundColor: '#19232D',
